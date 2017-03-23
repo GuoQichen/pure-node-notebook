e3e4450eccb7f3a67bb2f8d71c619da8100bd648
guoqichen <250915274@qq.com>
2017-03-23 22:36:43 +0800

    add public

--- app/index.js
+++ app/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const path = require('path')
 
 class App {
     constructor() {
@@ -9,7 +10,21 @@ class App {
         
 
         return (request, response) => {
-            response.end('hello world')
+
+            const mapUrlToFile = {
+                '/': 'public/index.html',
+                '/css/index.css': 'public/css/index.css',
+                '/js/index.js': 'public/js/index.js'
+            }
+
+            const url = mapUrlToFile[request.url] || ''
+
+            path.resolve('hello', 'hi')
+            fs.readFile(url, (error, data) => {
+                if(error) data = 'sorry, NOT FOUND'
+                response.end(data)
+            })
+            
         }
     }
 }
