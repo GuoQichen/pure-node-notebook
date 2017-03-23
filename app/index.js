e056b1b77867d6560dfedf85fde4cecfd52ecad2
guoqichen <250915274@qq.com>
2017-03-24 00:10:00 +0800

    update

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const path = require('path')
 
+const STATIC = 'public'
+
 class App {
     constructor() {
 
@@ -10,21 +12,26 @@ class App {
         
 
         return (request, response) => {
+            const { url } = request
 
-            const mapUrlToFile = {
-                '/': 'public/index.html',
-                '/css/index.css': 'public/css/index.css',
-                '/js/index.js': 'public/js/index.js'
+            const getUrl = url => { 
+                const staticPrex = path.resolve(process.cwd(), STATIC)
+                if(url === '/') url = '/index.html'    
+                return path.resolve(staticPrex, `.${url}`)
             }
-
-            const url = mapUrlToFile[request.url] || ''
-
-            path.resolve('hello', 'hi')
-            fs.readFile(url, (error, data) => {
+            
+            const _path = getUrl(url)
+            fs.readFile(_path, (error, data) => {
                 if(error) data = 'sorry, NOT FOUND'
                 response.end(data)
             })
-            
+            // const mapUrlToFile = {
+            //     '/': 'public/index.html',
+            //     '/css/index.css': 'public/css/index.css',
+            //     '/js/index.js': 'public/js/index.js'
+            // }
+
+            // const url = mapUrlToFile[request.url] || ''
         }
     }
 }
