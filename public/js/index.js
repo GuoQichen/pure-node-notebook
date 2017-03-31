69dfb8a3ea256d659dd02c6d3a809a85885ef0a3
guoqichen <250915274@qq.com>
2017-03-31 22:43:51 +0800

    modify

--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,12 @@
 window.onload = () => {
-    setTimeout(() => {
-        $.ajax('/user.action').done(data => {
-            const list = data.map(ele => `<li>${ele}</li>`)
-            $('#root').html(list)
-        }).fail(error => {
-            console.log(error)
-        })
-    }, 1000) 
+    // setTimeout(() => {
+    //     $.ajax('/user.action').done(data => {
+    //         const list = data.map(ele => `<li>${ele}</li>`)
+    //         $('#root').html(list)
+    //     }).fail(error => {
+    //         console.log(error)
+    //     })
+    // }, 1000) 
 
     $.ajax('/list.action').done(data => {
         const list = data.map(ele => `<li>${ele}</li>`)
