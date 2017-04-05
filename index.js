adf5671320ad254c567da0378626bacafcea6bf3
guoqichen <250915274@qq.com>
2017-04-05 23:20:34 +0800

    add view server

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const apiServer = require('./app/api')
 const staticServer = require('./app/static-server')
 const urlParser = require('./app/url-parser')
 const handleResponse = require('./app/handle-response')
+const viewServer = require('./app/view-server')
 
 const PORT = 7000
 
@@ -14,6 +15,7 @@ server.use(urlParser)
 server.use(apiServer)
 server.use(staticServer)
 server.use(handleResponse)
+server.use(viewServer)
 
 http.createServer(server.initServer()).listen(PORT, () => {
     console.log(`listen in ${PORT}`)
