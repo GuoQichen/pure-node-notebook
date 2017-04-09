5f01a3ac7fc17e5c831b2cd1e8668bee8cea7dc7
guoqichen <250915274@qq.com>
2017-04-09 13:53:19 +0800

    add lesson7

--- app/handle-response/index.js
+++ app/handle-response/index.js
@@ -1,6 +1,6 @@
 module.exports = context => {
     const { responseCtx, response } = context
-    const { headers, body } = responseCtx
-    response.writeHead(200, 'ok', Object.assign(headers, { 'X-powered-by': 'Node' }))
+    const { headers, body, statusCode, statusMessage } = responseCtx
+    response.writeHead(statusCode, statusMessage, Object.assign(headers, { 'X-powered-by': 'Node' }))
     response.end(body)
 }
--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+// window.onload = () => {
     // setTimeout(() => {
     //     $.ajax('/user.action').done(data => {
     //         const list = data.map(ele => `<li>${ele}</li>`)
@@ -8,22 +8,24 @@ window.onload = () => {
     //     })
     // }, 1000) 
 
-    $.ajax('/list.action').done(data => {
-        const list = data.map(ele => `<li>${ele}</li>`)
-        $('#shop').html(list)      
-    }).fail(error => {
-        console.log(error)
-    })
-    // post
+    // $.ajax('/list.action').done(data => {
+    //     const list = data.map(ele => `<li>${ele}</li>`)
+    //     $('#shop').html(list)      
+    // }).fail(error => {
+    //     console.log(error)
+    // })
+    // // post
 
-    $.ajax({
-        url: '/list.action',
-        method: 'post',
-        contentType: 'application/json',
-        dataType: 'json',
-        data:JSON.stringify(['d', 'e', 'f'])
-    }).done(data => {
-        const list = data.map(ele => `<li>${ele}</li>`)
-        $('body').append(list)             
-    })
-}
+    // $.ajax({
+    //     url: '/list.action',
+    //     method: 'post',
+    //     contentType: 'application/json',
+    //     dataType: 'json',
+    //     data:JSON.stringify(['d', 'e', 'f'])
+    // }).done(data => {
+    //     const list = data.map(ele => `<li>${ele}</li>`)
+    //     $('body').append(list)             
+    // })
+    document.write('hello')
+    console.log('hello ok?')
+// }
