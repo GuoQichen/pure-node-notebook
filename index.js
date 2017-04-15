092acb9bec49d143dc0bdab9562874fcfc17b2ae
guoqichen <250915274@qq.com>
2017-04-15 22:21:28 +0800

    add cookie login logout

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ const staticServer = require('./app/static-server')
 const urlParser = require('./app/url-parser')
 const handleResponse = require('./app/handle-response')
 const viewServer = require('./app/view-server')
+const cookieParser = require('./app/cookie-parser')
 
 const PORT = 7000
 
 const server = new App()
 
+server.use(cookieParser)
 server.use(urlParser)
 server.use(apiServer)
 server.use(staticServer)
